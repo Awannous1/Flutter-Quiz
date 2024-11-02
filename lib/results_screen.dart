6a9732224b5ed43768793350de53e535dae69636
Ahmad wannous <ahmadwanous642@gmail.com>
2024-11-02 15:45:34 +0200

    Add Screen Results

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
+
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i],
+        },
+      );
+    }
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -15,7 +40,7 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               width: 30,
             ),
-            const Text('List of answers and questions...'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(
               width: 30,
             ),
